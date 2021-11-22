efca57145803bd45b7ee112aa6d56816657a153a
TALATIZI <92215347+TALATIZI@users.noreply.github.com>
2021-11-22 17:22:58 +0100

    ss

--- tp3/JS/formulaire_validation.js
+++ tp3/JS/formulaire_validation.js
@@ -4,22 +4,22 @@ const firstNameInput = document.querySelector('input[name= "firstname"]');
 const lastNameInput = document.querySelector('input[name= "lastname"]');
 const emailInput = document.querySelector('input[name = "email"]');
 const dateInput = document.querySelector('input[name="date"]');
-const adressInput=document.querySelector('input[name="adress"]');
+const adressInput = document.querySelector('input[name="adress"]');
 
-let isFormValid = false ;
-let isValidationOn = false ;
+let isFormValid = false;
+let isValidationOn = false;
 
-const inputs = [firstNameInput,lastNameInput,adressInput,emailInput,dateInput];
+const inputs = [firstNameInput, lastNameInput, adressInput, emailInput, dateInput];
 
 
 // reset inputs if i already taped in
-const resetInputElm = (elm)=>{
+const resetInputElm = (elm) => {
     elm.classList.remove("invalid");
     elm.nextElementSibling.classList.add("hidden");
 };
 
 //  if input invalid so remove error
-const invalidateInputElm =(elm) =>{
+const invalidateInputElm = (elm) => {
     elm.classList.add("invalid"); // if invalid
     elm.nextElementSibling.classList.remove("hidden");
 };
@@ -27,75 +27,73 @@ const invalidateInputElm =(elm) =>{
 // for check the valiaton of the inputs
 const validateInputs = () => {
 
-    if(!isValidationOn) return;
+    if (!isValidationOn) return;
 
     // resetInputElm(firstNameInput);
     // resetInputElm(emailInput);
 
     inputs.forEach(resetInputElm);
-    isFormValid = true ;
+    isFormValid = true;
 
-    if(!firstNameInput.value || firstNameInput.value.length >= 20){
+    if (!firstNameInput.value || firstNameInput.value.length >= 20) {
         invalidateInputElm(firstNameInput);
-        isFormValid = false ;
+        isFormValid = false;
 
         console.log(firstNameInput.value);
     }
 
-    if(!lastNameInput.value || lastNameInput.value.length >= 20){
-         invalidateInputElm(lastNameInput);
-         isFormValid = false ;
+    if (!lastNameInput.value || lastNameInput.value.length >= 20) {
+        invalidateInputElm(lastNameInput);
+        isFormValid = false;
     }
-    if(!isValidEmail(adressInput.value)){
+    if (!isValidEmail(adressInput.value)) {
         invalidateInputElm(adressInput);
-        isFormValid = false ;
+        isFormValid = false;
     }
-    if(!isValidEmail(emailInput.value)){
+    if (!isValidEmail(emailInput.value)) {
         invalidateInputElm(emailInput);
-        isFormValid = false ;
+        isFormValid = false;
     }
 
-    if(! isValiddate(dateInput.value)){
-        
+    if (!isValiddate(dateInput.value)) {
+
         invalidateInputElm(dateInput);
         isFormValid = false;
     }
 
 };
 
+form.addEventListener("submit", (e) => {
 
-form.addEventListener("submit" ,(e)=> {
-
-   // e.preventdefault(); // not refrech the page
+    // e.preventdefault(); // not refrech the page
     e.preventDefault();
-    isValidationOn = true ;
+    isValidationOn = true;
     validateInputs();
 
-    if(isFormValid){
+    if (isFormValid) {
         form.remove();
-      succesMessage.classList.remove("hidden");
-        alert('thnks bro');
+        succesMessage.classList.remove("hidden");
+        alert('bonjour, firstname',);
+        alert('vous etre née le   et vous habitez href="https://maps.googleapis.com/maps/api/staticmap?markers=Paris&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"')
     }
 });
 // to catch errors in every input
-inputs.forEach((input) =>{
-input.addEventListener("input",(e)=>{
-    e.preventDefault();
-    validateInputs();
-});
+inputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        e.preventDefault();
+        validateInputs();
+    });
 });
 
 const isValidEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  };
-  
-  const isValidPhone = (phone) => {
-    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    return re.test(String(phone).toLowerCase());
-  };
-  const isValiddate =(date) => {
-    const re =/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
+};
+
+
+
+const isValiddate = (date) => {
+    const re = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
     return re.test(String(date).toLowerCase());
-  }
-  
+}
+
